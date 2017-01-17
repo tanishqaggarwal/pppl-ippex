b3709c9904fac5c0d058762f2e832e43721d0fe4
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2017-01-17 16:52:03 -0500

    The Day's Work

---
+++ static/js/index.js
@@ -0,0 +1,23 @@
+$(function() {
+	$('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
+});
+
+$(document).ready(function() {
+//	$('.parallax').parallax();
+	$(window).scroll(function() {
+	if (element.scrollTop < 100) {
+		return;
+	}
+});
+});
